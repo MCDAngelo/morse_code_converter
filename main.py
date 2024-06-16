b27b15c21e8df68020fe934cf650186a04ee42ad
MCDAngelo <dangelo.maria@gmail.com>
2024-06-16 14:43:48 -0600

    Add poetry

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from constants import MORSE_CODE_DICT, logo 
+from constants import MORSE_CODE_DICT, logo
+
 
 def translate(s):
     translated = [MORSE_CODE_DICT.get(l, "<missing code>") for l in s.upper()]
     return translated
+
 
 def run_translator():
     input_word = input("What word do you wish to convert to Morse Code?\n> ")
@@ -11,6 +13,7 @@ def run_translator():
     go_again = input("Do you want to convert another word? y or n\n> ")
     if go_again == "y":
         run_translator()
+
 
 if __name__ == "__main__":
     print(logo)
