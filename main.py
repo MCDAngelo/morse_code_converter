1582c93c7a1a44ea99a14a057308df13a256ac49
MCDAngelo <dangelo.maria@gmail.com>
2024-06-16 09:56:34 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from constants import MORSE_CODE_DICT, logo 
+
+def translate(s):
+    translated = [MORSE_CODE_DICT.get(l, "<missing code>") for l in s.upper()]
+    return translated
+
+def run_translator():
+    input_word = input("What word do you wish to convert to Morse Code?\n")
+    translated_word = translate(input_word)
+    print(f"In Morse Code, the word {input_word} is:\n {translated_word}")
+    go_again = input("Do you want to convert another word? y or n\n")
+    if go_again == "y":
+        run_translator()
+
+if __name__ == "__main__":
+    print(logo)
+    run_translator()
